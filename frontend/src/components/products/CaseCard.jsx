cfede1d34776be146ac0ef137c32ec36c9041510
mangelvl <thorristerki@gmail.com>
2023-06-30 15:28:06 -0400

    add: product list, card.

--- frontend/src/components/products/CaseCard.jsx
+++ frontend/src/components/products/CaseCard.jsx
@@ -1,24 +1,28 @@
-function CaseCard({data,index}) {
-    return(
-        <div className="group flex flex-col overflow-hidden rounded-lg shadow-lg">
-        <div className="flex-shrink-0">
-          <img id={data} className="h-96 w-full object-cover group-hover:object-scale-down hover:transition hover:duration-300 hover:ease-in-out" src={data.imageUrl} alt="" />
-        </div>
-        <div className="flex flex-1 flex-col justify-between bg-white p-6">
-          <div className="flex-1">
-            <p className="text-xl font-medium text-gray-900">
-              <a href={data.category.href} className="hover:underline">
-                {data.category.name}
-              </a>
-            </p>
-            <a href={data.href} className="mt-2 block">
-              <p id={index} className="lg:text-4xl pt-4 pb-6 text-2xl font-semibold transition duration-100 ease-in-out text-gray-900 group-hover:text-orange-500">{data.title}</p>
-              <p className="mt-3 text-2xl space-y-2 leading-9 text-gray-500">{data.description}</p>
-            </a>
-          </div>
+function CaseCard({ data, index }) {
+  return (
+    <article className="group bg-white flex flex-col w-full drop-shadow-md mb-4 md:w-56 lg:w-64 xl:w-72 2xl:w-80">
+      <div className="">
+        <div className="relative h-full lg:h-48 overflow-hidden">
+          <img id={data} className="group-hover:scale-110 transition duration-300 ease-in-out" src={data.imageUrl} alt="" />
         </div>
       </div>
-    )
+      <div className="flex flex-1 flex-col justify-between p-6">
+        <div className="">
+          <a href={data.href} className="">
+            <h3 id={index} className="text-3xl font-semibold transition duration-100 ease-in-out text-black group-hover:text-orange-500 md:text-xl">
+              {data.title}
+            </h3>
+            <p className="text-lg space-y-2 leading-9 text-gray-700">
+              {data.author.name}
+            </p>
+            <p className="text-lg space-y-2 leading-9 text-gray-700">
+              {data.date}
+            </p>
+          </a>
+        </div>
+      </div>
+    </article>
+  )
 }
 
 export default CaseCard
